a5b46af67499a479d5c7a7cd3255ea854b922127
Margarita Novokh <r28611@gmail.com>
2021-11-30 21:34:19 +0100

    added gamep logics

--- Ping-Pong/GameViewController.m
+++ Ping-Pong/GameViewController.m
@@ -41,6 +41,19 @@
     [user readResultsFromFile];
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self becomeFirstResponder];
+    [self newGame];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [self resignFirstResponder];
+}
+
 - (void)setupGrid {
     _gridView = [[UIView alloc] initWithFrame:CGRectMake(0, HALF_SCREEN_HEIGHT - 2, SCREEN_WIDTH, 4)];
     _gridView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.5];
@@ -142,6 +155,121 @@
 
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     [self touchesEnded:touches withEvent:event];
+}
+
+- (void)displayMessage:(NSString *)message {
+    [self stop];
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Ping Pong" message:message preferredStyle:(UIAlertControllerStyleAlert)];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
+        if ([self gameOver]) {
+            [self newGame];
+            return;
+        }
+        [self reset];
+        [self start];
+    }];
+    [alertController addAction:action];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+- (void)newGame {
+    [self reset];
+    
+    _scoreTop.text = @"0";
+    _scoreBottom.text = @"0";
+    
+    [self displayMessage:@"Готовы к игре?"];
+}
+
+- (int)gameOver {
+    if ([_scoreTop.text intValue] >= MAX_SCORE) return 1;
+    if ([_scoreBottom.text intValue] >= MAX_SCORE) return 2;
+    return 0;
+}
+
+- (void)start {
+    _ball.center = CGPointMake(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT);
+    if (!_timer) {
+        _timer = [NSTimer scheduledTimerWithTimeInterval:1.0/60.0 target:self selector:@selector(animate) userInfo:nil repeats:YES];
+    }
+    _ball.hidden = NO;
+}
+
+- (void)reset {
+    if ((arc4random() % 2) == 0) {
+        _dx = -1;
+    } else {
+        _dx = 1;
+    }
+    
+    if (_dy != 0) {
+        _dy = -_dy;
+    } else if ((arc4random() % 2) == 0) {
+        _dy = -1;
+    } else  {
+        _dy = 1;
+    }
+    
+    _ball.center = CGPointMake(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT);
+    
+    _speed = 2;
+}
+
+- (void)stop {
+    if (_timer) {
+        [_timer invalidate];
+        _timer = nil;
+    }
+    _ball.hidden = YES;
+}
+
+- (void)animate {
+    _ball.center = CGPointMake(_ball.center.x + _dx*_speed, _ball.center.y + _dy*_speed);
+    [self checkCollision:CGRectMake(0, 0, 20, SCREEN_HEIGHT) X:fabs(_dx) Y:0];
+    [self checkCollision:CGRectMake(SCREEN_WIDTH, 0, 20, SCREEN_HEIGHT) X:-fabs(_dx) Y:0];
+    if ([self checkCollision:_paddleTop.frame X:(_ball.center.x - _paddleTop.center.x) / 32.0 Y:1]) {
+        [self increaseSpeed];
+    }
+    if ([self checkCollision:_paddleBottom.frame X:(_ball.center.x - _paddleBottom.center.x) / 32.0 Y:-1]) {
+        [self increaseSpeed];
+    }
+    [self goal];
+}
+
+- (void)increaseSpeed {
+    _speed += 0.5;
+    if (_speed > 10) _speed = 10;
+}
+
+- (BOOL)checkCollision: (CGRect)rect X:(float)x Y:(float)y {
+    if (CGRectIntersectsRect(_ball.frame, rect)) {
+        if (x != 0) _dx = x;
+        if (y != 0) _dy = y;
+        return YES;
+    }
+    return NO;
+}
+
+- (BOOL)goal
+{
+    if (_ball.center.y < 0 || _ball.center.y >= SCREEN_HEIGHT) {
+        int s1 = [_scoreTop.text intValue];
+        int s2 = [_scoreBottom.text intValue];
+        
+        if (_ball.center.y < 0) ++s2; else ++s1;
+        _scoreTop.text = [NSString stringWithFormat:@"%u", s1];
+        _scoreBottom.text = [NSString stringWithFormat:@"%u", s2];
+        
+        int gameOver = [self gameOver];
+        if (gameOver) {
+            [self displayMessage:[NSString stringWithFormat:@"Игрок %i выиграл", gameOver]];
+        } else {
+            [self reset];
+        }
+        
+        return YES;
+    }
+    return NO;
 }
 
 @end
