011d9e1f6b22aeb131b9ad748e0938ee529ca2c0
Margarita Novokh <r28611@gmail.com>
2021-12-02 13:45:06 +0100

    application lifecycle

--- Ping-Pong/AppDelegate.m
+++ Ping-Pong/AppDelegate.m
@@ -15,10 +15,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    NSLog(@"application launch");
     return YES;
 }
 
+- (void)applicationWillTerminate:(UIApplication *)application {
+    NSLog(@"application terminate");
+}
 
 #pragma mark - UISceneSession lifecycle
 
@@ -34,6 +37,7 @@
     // Called when the user discards a scene session.
     // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
     // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    NSLog(@"user discards a scene session");
 }
 
 
